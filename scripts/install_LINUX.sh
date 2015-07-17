b00d911c480837b197e466207a7c42a6efe41d51
Navaneeth Rao <navaneetha@ariem2.com>
2015-07-17 17:14:05 +0530

    script for MAC and LINUX(ubuntu)

---
+++ scripts/install_LINUX.sh
@@ -0,0 +1,31 @@
+## script to install mongodb , node.js , pisignage-server
+## for more info http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+
+DBDIR="/data/db"
+
+echo " installing mongodb"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+sudo apt-get -y update
+sudo apt-get install -y mongodb-org
+
+# check /data/db directory present if not create
+if [ ! -d "$DBDIR" ];then
+	sudo mkdir -p /data/db
+fi
+#chagne permission
+sudo chmod -R 755 /data/
+
+# From https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+echo " installing Node.js"
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get install -y nodejs
+
+
+echo "installing pisignage-server"
+git clone https://github.com/ariemtech/pisignage-server 
+cd pisignage-server
+npm install
+
---
+++ scripts/install_MAC.sh
@@ -0,0 +1,27 @@
+# Run this script as root user 'sudo install_MAC.sh'
+# Please install HomeBrew if not present from  "http://brew.sh/"
+
+DBDIR="/data/db"
+
+#install mongo on mac
+echo " installing mongodb "
+brew update
+brew install mongodb
+
+#check /data/db directory present if not create
+if [ ! -d "$DBDIR" ];then
+	sudo mkdir -p /data/db
+fi
+#chagne permission
+sudo chmod -R 755 /data/
+
+# homebrew node.js package 
+echo "installing node.js"
+brew install node
+
+echo "installing pisignage-server"
+git clone https://github.com/ariemtech/pisignage-server 
+cd pisignage-server
+npm install
+
+echo " pisignage-server is ready run `sudo node server.js`"
