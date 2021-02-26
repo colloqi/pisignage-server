6f5ad4f955e067ead51ed76fc8baa5c7c5008ed7
Ravi Bail <ravi@colloqiconsulting.com>
2021-02-26 12:23:03 +0530

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -5,7 +5,9 @@ module.exports = {
     https: false,
     port: process.env.PORT || 3000,
     mongo: {
-        uri: 'mongodb://mongo:27017/pisignage-server-dev' ,
+        uri: process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            'mongodb://mongo:27017/pisignage-server-dev',
         options: {useMongoClient: true}
     }
 };
